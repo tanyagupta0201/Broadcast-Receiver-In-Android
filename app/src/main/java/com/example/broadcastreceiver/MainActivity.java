eef5e294e6d321311ebae7931c85f4aa3bbf9e7c
Tanya Gupta <tanyagupta.pg@gmail.com>
2022-03-29 19:43:14 +0530

    First Commit

---
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if(Intent.ACTION_SEND.equals(action) && type != null)
+        {
+             imageView.setImageURI(intent.getParcelableExtra(Intent.EXTRA_STREAM));
+        }
+    }
+}
